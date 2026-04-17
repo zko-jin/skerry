ce9272c9a538ced8516fba31c9eefb2b6a150520
zko-jin <zkojin@proton.me>
2026-04-17 10:42:43 -0300

    feat: skerry_trait

---
+++ skerry_macros/src/internal/skerry_trait.rs
@@ -0,0 +1,53 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{ItemTrait, TraitItem, TraitItemFn, parse_macro_input};
+
+use crate::internal::skerry_impl::ImplAttribute;
+
+pub fn skerry_trait(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input_trait = parse_macro_input!(item as ItemTrait);
+    let mut top_level_code = quote! {};
+    let attr_args = syn::parse_macro_input!(attr as ImplAttribute);
+
+    for item in &mut input_trait.items {
+        if let TraitItem::Fn(method) = item {
+            if let Some(pos) = method
+                .attrs
+                .iter()
+                .position(|a| a.path().is_ident("skerry_fn"))
+            {
+                method.attrs.remove(pos);
+
+                let method_tokens = quote!(#method).into();
+
+                match crate::internal::skerry_fn::skerry_fn_trait(
+                    TokenStream::new(),
+                    method_tokens,
+                    attr_args.prefix.as_ref(),
+                ) {
+                    Ok((top_stream, method_stream)) => {
+                        // 1. Add the generated Enum/Macro call to the top-level bucket
+                        top_level_code.extend(top_stream);
+
+                        // 2. Replace the current method with the transformed one
+                        // We parse it back into an TraitItemFn
+                        match syn::parse2::<TraitItemFn>(method_stream) {
+                            Ok(new_method) => *method = new_method,
+                            Err(e) => return e.to_compile_error().into(),
+                        }
+                    }
+                    Err(compile_error) => return compile_error,
+                }
+            }
+        }
+    }
+
+    // Wrap everything back up
+    let expanded = quote! {
+        #top_level_code
+
+        #input_trait
+    };
+
+    TokenStream::from(expanded)
+}
